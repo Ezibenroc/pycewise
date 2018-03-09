d6d6d04b299d1192891f6a5c5e04c1648feacf03
Tom Cornebize <tom.cornebize@univ-grenoble-alpes.fr>
2018-03-09 20:38:40 +0100

    Tentative fix.

--- pytree.py
+++ pytree.py
@@ -1,12 +1,54 @@
 import numpy
 
+class IncrementalStat:
+    '''See https://en.wikipedia.org/wiki/Algorithms_for_calculating_variance#Computing_shifted_data'''
+    def __init__(self):
+        self.values = []
+        self.K = 0
+        self.Ex = 0
+        self.Ex2 = 0
+
+    def __len__(self):
+        return len(self.values)
+
+    def add(self, val):
+        if len(self) == 0:
+            self.K = val
+        self.values.append(val)
+        self.Ex  += val - self.K
+        self.Ex2 += (val - self.K)**2
+
+    @property
+    def last(self):
+        return self.values[-1]
+
+    def pop(self):
+        val = self.values.pop()
+        self.Ex  -= val - self.K
+        self.Ex2 -= (val - self.K)**2
+        return val
+
+    @property
+    def mean(self):
+        return self.K + self.Ex/len(self)
+
+    @property
+    def var(self):
+        return (self.Ex2 - (self.Ex**2)/len(self)) / len(self)
+
+    @property
+    def std(self):
+        return self.var ** (1/2)
+
 class Node:
     def __init__(self, x, y):
         '''Assume the values in x are sorted in increasing order.'''
         assert len(x) == len(y)
-        self.x = x
-        self.y = y
-        self.compute_values()
+        self.x = IncrementalStat()
+        self.y = IncrementalStat()
+        self.cov_sum = IncrementalStat()
+        for xx, yy in zip(x, y):
+            self.add(xx, yy)
 
     def __len__(self):
         return len(self.x)
@@ -14,41 +56,25 @@ class Node:
     def __str__(self):
         return 'y ~ %3.fx + %.3f | R^2 = %.3f' % (self.beta, self.alpha, self.rsquared)
 
-    @staticmethod
-    def square_sum(values, mean_val):
-        s = 0
-        for v in values:
-            s += (v - mean_val)**2
-        return s
-
-    def compute_values(self):
-        self.sum_x = sum(self.x)
-        self.sum_y = sum(self.y)
-        self.square_sum_x = self.square_sum(self.x, self.mean_x)
-        self.square_sum_y = self.square_sum(self.y, self.mean_y)
-        self.cov_sum = 0
-        for x, y in zip(self.x, self.y):
-            self.cov_sum += (x-self.mean_x)*(y-self.mean_y)
-
     @property
     def mean_x(self):
-        return self.sum_x / len(self)
+        return self.x.mean
 
     @property
     def mean_y(self):
-        return self.sum_y / len(self)
+        return self.y.mean
 
     @property
     def std_x(self):
-        return (self.square_sum_x/len(self))**(1/2)
+        return self.x.std
 
     @property
     def std_y(self):
-        return (self.square_sum_y/len(self))**(1/2)
+        return self.y.std
 
     @property
     def cov(self):
-        return self.cov_sum / len(self)
+        return self.cov_sum.mean
 
     @property
     def corr(self):
@@ -67,23 +93,14 @@ class Node:
         return self.corr**2
 
     def add(self, x, y):
-        assert x >= self.x[-1]
-        self.x.append(x)
-        self.y.append(y)
-        self.sum_x += x
-        self.sum_y += y
-        self.square_sum_x += (x-self.mean_x)**2
-        self.square_sum_y += (y-self.mean_y)**2
-        self.cov_sum += (x-self.mean_x)*(y-self.mean_y)
+        self.x.add(x)
+        self.y.add(y)
+        self.cov_sum.add((x-self.mean_x)*(y-self.mean_y))
 
     def pop(self):
-        x = self.x[-1]
-        y = self.y[-1]
-        self.cov_sum -= (x-self.mean_x)*(y-self.mean_y)
-        self.square_sum_y -= (y-self.mean_y)**2
-        self.square_sum_x -= (x-self.mean_x)**2
-        self.sum_y -= y
-        self.sum_x -= x
+        x = self.x.last()
+        y = self.y.last()
+        self.cov_sum.pop()
         self.x.pop()
         self.y.pop()
         return x, y
--- test.py
+++ test.py
@@ -1,7 +1,24 @@
 import unittest
 import random
 import numpy
-from pytree import Node
+from pytree import Node, IncrementalStat
+
+class IncrementalStatTest(unittest.TestCase):
+    def test_basic(self):
+        size = random.randint(50, 100)
+        values = []
+        stats = IncrementalStat()
+        for _ in range(size):
+            val = random.uniform(0, 100)
+            stats.add(val)
+            values.append(val)
+        for _ in range(size-2): # don't do the last two ones
+            val = stats.last
+            self.assertEqual(stats.pop(), val)
+            self.assertEqual(values.pop(), val)
+            self.assertAlmostEqual(numpy.mean(values), stats.mean)
+            self.assertAlmostEqual(numpy.var(values),  stats.var)
+            self.assertAlmostEqual(numpy.std(values),  stats.std)
 
 
 class NodeTest(unittest.TestCase):
@@ -35,6 +52,23 @@ class NodeTest(unittest.TestCase):
             y = [d[1] + random.gauss(0, noise) for d in self.data]
             node = Node(x, y)
             self.perform_tests(x, y, node, noise > 0)
+
+
+    def test_add(self):
+        for noise in [0, 1, 2, 4, 8]:
+            x = [d[0] for d in self.data]
+            y = [d[1] + random.gauss(0, noise) for d in self.data]
+            limit = self.size // 3
+            new_x = x[:limit]
+            new_y = y[:limit]
+            node = Node(list(new_x), list(new_y))
+            self.perform_tests(new_x, new_y, node, noise > 0)
+            for xx, yy in zip(x[limit:], y[limit:]):
+                print('bla')
+                node.add(xx, yy)
+                new_x.append(xx)
+                new_y.append(yy)
+                self.perform_tests(new_x, new_y, node, noise > 0)
 
 
 if __name__ == "__main__":
